33f3692e1e3e7bf6b9ff5ae8d7b5dfa2d1941b38
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-07-25 15:43:28 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,14 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.post("/post-message", async (req, res, next) => {
+router.post("/post-message", async (req, res) => {
   try {
-    const clientMessage = await req.body.clientMessage;
     const dateTime = new Date();
-    res.json(
-      `Received client message: ${clientMessage}. Responded at ${dateTime.toString()}`
-    );
+    const clientMessage = req.body.clientMessage;
+    const response = `Received client message: ${clientMessage}. Responded at ${dateTime.toString()}`;
+    res.json({ serverMessage: response }).status(200);
   } catch (error) {
     res.json({ success: false }).status(500);
   }
 });
-
 module.exports = router;
