feebb274e8ed818ebcecad82ae479a37672021c5
Jill Kidman <jillkidman@gmail.com>
2022-07-27 09:06:16 -0700

    dev branch commit

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 var express = require("express");
 var router = express.Router();
 
+const userList = [{
+  id: 1,
+  firstName: "John",
+  lastName: "Doe",
+  email: "jd@gmail.com"
+}];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
+
+// GET users
+router.get('/get-users', function (req, res) {
+  res.json(userList);
+})
+
 
 router.post("/post-message", async (req, res) => {
   try {
